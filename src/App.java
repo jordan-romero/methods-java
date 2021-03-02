0934aa3807f7a8b11bea6e98f1b4ac76bd70217c
jordles113 <jordan.t.romero@gmail.com>
2021-03-02 14:30:29 -0600

    finishes challenge

--- src/App.java
+++ src/App.java
@@ -1,18 +1,42 @@
 public class App {
     public static void main(String[] args) {
-        boolean gameOver = true; 
+        boolean gameOver = false; 
         
-       calculateScore(gameOver, 800, 5, 100);
+       int highScore = calculateScore(gameOver, 800, 5, 100);
+
+        int position = calculateHighScore(highScore);
        
-       calculateScore(gameOver, 10000, 8, 200);
+       highScore = calculateScore(gameOver, 10000, 8, 200);
+
+       position = calculateHighScore(highScore);
+
+       displayHighScorePosition("Jordan", position); 
+
         
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) { 
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) { 
 
         if (gameOver == true) {
             int finalScore = score + (levelCompleted * bonus); 
-            System.out.println("Your final score was " + finalScore);
+            return finalScore; 
         }
+        return -1; 
+    }
+
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " managed to get into position " + position + " on the highscore table.");
+    }
+
+    public static int calculateHighScore(int highScore) {
+       if (highScore > 1000) {
+           return 1; 
+       } else if (highScore > 500 && highScore < 1000) {
+           return 2; 
+       } else if (highScore > 100 && highScore < 500) {
+           return 3;
+       } else {
+           return 4; 
+       }
     }
 }
