908f3ea8d00befc43c8ac6d97bea9eba3aa7980d
jordles113 <jordan.t.romero@gmail.com>
2021-03-02 14:08:47 -0600

    passing parameters to methods

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) {
+        boolean gameOver = true; 
+       int score = 4000; 
+       int levelCompleted = 5; 
+       int bonus = 100; 
+
         
+       calculateScore(true, score, levelCompleted, bonus);
+        score = 10000; 
+        levelCompleted = 8; 
+        bonus = 200; 
+       calculateScore(true, score, levelCompleted, bonus);
+        
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) { 
+
+        if (gameOver == true) {
+            int finalScore = score + (levelCompleted * bonus); 
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }
