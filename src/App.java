582d90a36e9d1d8c68aa67ec992c8cfcffb2d589
jordles113 <jordan.t.romero@gmail.com>
2021-03-02 14:10:26 -0600

    cleans up code

--- src/App.java
+++ src/App.java
@@ -1,16 +1,10 @@
 public class App {
     public static void main(String[] args) {
         boolean gameOver = true; 
-       int score = 4000; 
-       int levelCompleted = 5; 
-       int bonus = 100; 
-
         
-       calculateScore(true, score, levelCompleted, bonus);
-        score = 10000; 
-        levelCompleted = 8; 
-        bonus = 200; 
-       calculateScore(true, score, levelCompleted, bonus);
+       calculateScore(gameOver, 800, 5, 100);
+       
+       calculateScore(gameOver, 10000, 8, 200);
         
     }
 
